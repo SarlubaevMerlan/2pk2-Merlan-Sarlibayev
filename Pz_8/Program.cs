fa52be25978e497f01f8ecd0f88c1c0374d5ede5
SarlubaevMerlan <overfink@mail.ru>
2021-10-14 12:38:49 +0500

    Yes

---
+++ Pz_8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] a = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i]+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(a.Sum());
+            double proiz = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                proiz *= a[i];
+            }
+            Console.WriteLine(proiz);
+        }
+    }
+}
