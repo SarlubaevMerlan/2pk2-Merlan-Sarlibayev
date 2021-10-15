78a688794835bfc13a29185df840e482b2cb28c5
SarlubaevMerlan <overfink@mail.ru>
2021-10-15 21:36:01 +0500

    Здравствуйте

---
+++ Pz_9/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[10, 10];
+            int[] result = new int[10];
+            var random = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    array[i, j] = random.Next(100);
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    result[i] = array[i, j];
+                }
+            }
+
+            foreach (int a in result)
+            {
+                Console.WriteLine(a / 10);
+            }
+        }
+    }
+}
