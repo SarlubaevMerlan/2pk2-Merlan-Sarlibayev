1ee39ebaa224811fe5b40033d4e090e177a57ab7
SarlubaevMerlan <overfink@mail.ru>
2021-11-11 09:20:18 +0500

    пз_11

---
+++ Pz_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pz_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine().ToUpper();
+            string[] array = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach(string i in array)
+            {
+                if (i[0] == 'С') count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
