351cc0ee50e7d893faa08abde2343a495b02a675
SarlubaevMerlan <overfink@mail.ru>
2021-11-12 15:01:49 +0500

    Pz_12

---
+++ Pz_12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pz_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(". ");
+            for(int i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i].Length < str[i + 1].Length)
+                {
+                    (str[i], str[i + 1]) = (str[i + 1], str[i]);
+                }
+            }
+            Console.WriteLine(string.Join(". ", str));
+        }
+    }
+}
