bfb1047d21e87dcff8425b7cc5032e5f1543d448
SarlubaevMerlan <overfink@mail.ru>
2021-12-01 11:10:38 +0500

    Zdravstvuete

---
+++ Pz_13/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Pz_13
+{
+    class Program
+    {
+        [Flags()]
+        public enum Values
+        {
+            Verybad = 1,
+            Unsatisfactory = 2,
+            Satisfactorily = 3,
+            Good = 4,
+            Great = 5,
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите оценку по пяти бальной шкале: ");
+
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Ты поработал: ");
+            switch (a)
+            {
+                case (int)Values.Verybad:
+                    Console.WriteLine("Очень плохо");
+                    break;
+
+                case (int)Values.Unsatisfactory:
+                    Console.WriteLine("Неудовлетворительно");
+                    break;
+
+                case (int)Values.Satisfactorily:
+                    Console.WriteLine("Удовлитварительно");
+                    break;
+
+                case (int)Values.Good:
+                    Console.WriteLine("Хорошо");
+                    break;
+
+                case (int)Values.Great:
+                    Console.WriteLine("Отлично");
+                    break;
+            }
+        }
+    }
+}
---
+++ Pz_17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Pz_17
+{
+    class Program
+    {
+        static float IMT(float m,float height)
+        {
+            return m / height / 100 * height / 100;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свою массу: ");
+            float m = float.Parse(Console.ReadLine());
+            Console.Write("Введите свой рост: ");
+            float hight = float.Parse(Console.ReadLine());
+            Console.Write("Ваш индекс массы: " + IMT(m, hight));
+        }
+    }
+}
